8fa661c8f1a2564f4c3f504145bf3a88f1a1d40e
sam <ponnusamykrish23@gmail.com>
2023-05-12 18:37:05 +0530

    string

---
+++ challenges/character.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    char s[20];
+    char sen[30];
+
+
+    scanf("%c", &ch);
+    scanf("%s",s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s", sen);
+
+    return 0;
+}
