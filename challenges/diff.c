198a3d932b07af91e436cae70881eb965a8dead0
ponnusamy <ponnusamykrish23@gmail.com>
2023-05-22 11:52:03 +0530

    completed 3 challenges

---
+++ challenges/diff.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t); // Use scanf instead of cin in C
+
+    while (t--)
+    {
+        int A, C;
+        scanf("%d %d", &A, &C); // Use scanf instead of cin in C
+        
+        printf("%d %d\n",A,C);
+
+    }
+
+    return 0;
+}
---
+++ ptr/ptr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    int *p = &a;
+    printf("%d",3-4);
+
+    return 0;
+}
